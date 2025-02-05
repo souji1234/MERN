a5f955219fd4fd867185f71a825b5e9a570d07a7
souji1234 <125633690+souji1234@users.noreply.github.com>
2025-02-05 22:22:06 +0530

    javascript

---
+++ practice/javascript/myscript.js
@@ -0,0 +1,4 @@
+window.document.write["welcome to javascript programming...<br>"]
+window.document.write("welcome to javascript progrming...<br>");
+document.write("hello javascript");
+console.log("hello javascript");
